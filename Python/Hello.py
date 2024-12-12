5b20840a21df01cf6ff327861635e825566a94e5
njambiEvelyne <evelynenganga76@gmail.com>
2024-12-12 23:35:55 +0300

    Checking for conflicts

--- Python/Hello.py
+++ Python/Hello.py
@@ -1,3 +1,11 @@
 #This the first message to my GitHub
 print("Hello World!")
 
+# Collecting of the user input
+name = input("Enter your name")
+age = int(input("Enter your age:"))
+
+# Printing of the user's input details
+print(f"Hello {name}. Wecome to the system. You are {age} years old")
+
+
